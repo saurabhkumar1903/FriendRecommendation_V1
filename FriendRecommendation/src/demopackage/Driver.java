ab67ffb515076acc779dfa93146002f35ea064d5
hadoopuser <hadoopuser@saurabh-HP-Pavilion-Notebook>
2018-01-08 12:50:27 +0530

    Commit Version 1

---
+++ FriendRecommendation/src/demopackage/Driver.java
@@ -0,0 +1,38 @@
+
+package demopackage;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class Driver {
+ 
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = new Job(conf, "FriendRecommendation");
+        job.setJarByClass(Driver.class);
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(FriendCountWritable.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileSystem outFs = new Path(args[1]).getFileSystem(conf);
+        outFs.delete(new Path(args[1]), true);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
